3b09bc0e808738270ee5bedd9d4f00f5388786c8
victorbonifac10 <victor.alvesbonifacio@gmail.com>
2025-06-02 22:58:37 -0300

    Finalizacao do css e desenvolvimento do script

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//numeros
+
+const number1 = document.querySelector(".um").value
+const number2 = document.querySelector(".dois").value
+const number3 = document.querySelector(".tres").value
+const number4 = document.querySelector(".quatro").value
+const number5 = document.querySelector(".cinco").value
+const number6 = document.querySelector(".seis").value
+const number7 = document.querySelector(".sete").value
+const number8 = document.querySelector(".oito").value
+const number9 = document.querySelector(".nove").value
+const number0 = document.querySelector(".zero").value
+
+//operadores
+
+const btnAdicao = document.querySelector(".adicao").value
+const btnSubtracao = document.querySelector(".subtracao").value
+const btnDivisao = document.querySelector(".divisao").value
+const btnMultiplicao = document.querySelector(".multiplicacao").value
+
+function operation() {
+
+}
